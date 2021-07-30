4c43392db7ce491b15fad34e9c4f95c813e95151
Hotiakov <54938333+Hotiakov@users.noreply.github.com>
2021-07-30 11:39:14 +0300

    done

---
+++ js/script.js
@@ -0,0 +1,12 @@
+let num = 266219;
+
+let tmpString = num + "";
+let result = 1;
+
+for (let i = 0; i < tmpString.length; i++) {
+    result *= Number(tmpString[i]);
+}
+
+result **= 3;
+
+console.log(result.toString().substr(0, 2));
