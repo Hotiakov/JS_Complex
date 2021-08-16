ac6fedb4830f3c4973d610704278a4eb3665926a
Hotiakov <54938333+Hotiakov@users.noreply.github.com>
2021-08-16 15:16:43 +0300

    fixes

--- js/script.js
+++ js/script.js
@@ -21,8 +21,32 @@ let LogInBnt = document.querySelector(".LogIn__bnt");
 
 let adminCell = dataBase.querySelector(".admin");
 let curUser = localStorage.getItem("curUser") || "anonymous";
-let text = curUser === "anonymous" ? "Анонимный Боб" : JSON.parse(localStorage.getItem(curUser)).firstName;
-title.textContent = "Добро пожаловать, " + text;
+let text;
+
+
+let users;
+
+function replacer(key, value) {
+    if (value instanceof Map) {
+        return {
+            dataType: 'Map',
+            value: Array.from(value.entries()), // or with spread: value: [...value]
+        };
+    } else {
+        return value;
+    }
+}
+function reviver(key, value) {
+    if (typeof value === 'object' && value !== null) {
+        if (value.dataType === 'Map') {
+            return new Map(value.value);
+        }
+    }
+    return value;
+}
+
+
+
 
 tabLogIn.addEventListener('click', function () {
     contentWrapper.querySelector(".active").classList.remove("active");
@@ -56,7 +80,7 @@ tabRegistration.addEventListener('click', function () {
 const createLog = function (login) {
     let newElem = document.createElement("div");
     newElem.className = "dataBase__table_node dataBase__table_cell";
-    let fromLocal = JSON.parse(localStorage.getItem(login));
+    let fromLocal = users.get(login);
     let date = new Date(fromLocal.date);
     newElem.insertAdjacentHTML("afterbegin", `
         <span class="dataBase__table_firstName">${fromLocal.firstName}</span>
@@ -74,25 +98,29 @@ const createLog = function (login) {
     newBtn.textContent = "удалить пользователя";
     function remove() {
         newElem.remove();
-        newBtn.removeEventListener('click', null);
-        localStorage.removeItem(login, remove);
+        newBtn.removeEventListener('click', remove);
+        users.delete(login);
+        localStorage.setItem("users", JSON.stringify(users, replacer));
     }
     newBtn.addEventListener('click', remove);
 };
 
 const start = function () {
-    localStorage.setItem("admin", JSON.stringify({
-        login: "admin",
-        password: "admin",
-        firstName: "Админ",
-        secondName: "Админович"
-
-    }));
-    for (let i = 0; i < localStorage.length; i++) {
-        let key = localStorage.key(i);
-        if (key === "admin" || key === "curUser")
+    users = JSON.parse(localStorage.getItem("users"), reviver) || new Map();
+    if (!users.has("admin")) {
+        users.set("admin", {
+            login: "admin",
+            password: "admin",
+            firstName: "Админ",
+            secondName: "Админович"
+        });
+    }
+    text = ((curUser === "anonymous") ? "Анонимный Боб" : users.get(curUser).firstName);
+    title.textContent = "Добро пожаловать, " + text;
+    for (let key of users.keys()) {
+        if (key === "admin")
             continue;
-        createLog(JSON.parse(localStorage.getItem(key)).login);
+        createLog(key);
     }
 };
 
@@ -104,25 +132,26 @@ registrationBnt.addEventListener('click', function (e) {
         return;
     }
     let loginValue = registrationLogin.value;
-    if (localStorage.getItem(loginValue)) {
+    if (users.has(loginValue)) {
         alert("Пользователь с таким логином уже существует!");
         return;
     }
     let passwordValue = registrationPassword.value;
     let _date = new Date();
-
-    localStorage.setItem(loginValue, JSON.stringify({
+    users.set(loginValue, {
         firstName: nameValue[0],
         lastName: nameValue[1],
         login: loginValue,
         password: passwordValue,
         date: _date.getTime()
-    }));
+    });
+    localStorage.setItem("users", JSON.stringify(users, replacer));
     createLog(loginValue);
     registrationName.value = '';
     registrationLogin.value = '';
     registrationPassword.value = '';
 });
+
 LogInBnt.addEventListener('click', function (e) {
     e.preventDefault();
     let loginValue = LogInLogin.value || null;
@@ -131,11 +160,11 @@ LogInBnt.addEventListener('click', function (e) {
         alert("Не введен логи или пароль!!");
         return;
     }
-    if (localStorage.getItem(loginValue) === null) {
+    if (!users.has(loginValue)) {
         alert("Пользователя с ником " + loginValue + " не существует!");
         return;
     }
-    let user = JSON.parse(localStorage.getItem(loginValue));
+    let user = users.get(loginValue);
     if (user.password !== passwordValue) {
         alert("Неверно введен пароль!");
         return;
